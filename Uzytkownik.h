a92355df5027e57c3b54582a3fb782e29213c7ec
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2021-01-08 21:48:40 +0100

    Add registered user to vector

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -5,7 +5,17 @@ using namespace std;
 class Uzytkownik
 {
     int id;
-    string login, haslo;
+    string login;
+    string haslo;
+
+public:
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
 
 
 
